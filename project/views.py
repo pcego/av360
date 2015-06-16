28de0de534f024250648efd5a4f28f4dec7df52c
pcego <pcego36@gmail.com>
2015-06-16 00:02:52 -0300

    iniciando projeto

--- project/views.py
+++ project/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 
 
 def home(request):
-	return HttpResponse('Hello world from django 1.8 on Open Shift')
+	return HttpResponse('Hello world from pcego django 1.8 on Open Shift')
