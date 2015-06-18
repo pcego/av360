39b11e021112e7a25b89e5ccba8e0737658c7027
pcego <pcego36@gmail.com>
2015-06-17 23:41:25 -0300

    bugfix #003

--- project/settings.py
+++ project/settings.py
@@ -36,7 +36,7 @@ if ON_PAAS and DEBUG:
 TEMPLATE_DEBUG = True
 
 if ON_PAAS:
-    ALLOWED_HOSTS = ['*']
+    ALLOWED_HOSTS = [os.environ['OPENSHIFT_APP_DNS'], socket.gethostname()]
 else:
     ALLOWED_HOSTS = ['*']
 
