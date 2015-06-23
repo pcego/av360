463bd6b5c4327571d4455a2c25e5517fc3419cfa
pcego <pcego36@gmail.com>
2015-06-23 20:19:33 -0300

    creating aplication

--- project/settings.py
+++ project/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bootstrapform',
+    'ava360'
 )
 
 MIDDLEWARE_CLASSES = (
--- project/urls.py
+++ project/urls.py
@@ -1,52 +1,36 @@
-"""project URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.8/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Add an import:  from blog import urls as blog_urls
-    2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
-"""
 from django.conf.urls import include, url
-from django.contrib import admin, auth
+from django.contrib import admin
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
+
 
 urlpatterns = [
 
-	url(r'^$', 'django.contrib.auth.views.login',
+    url(r'^$', 'django.contrib.auth.views.login',
             {'template_name': 'index.html'}, name='url_login'),
 
-    	url(r'^register$', 'project.views.register', name='register'),
+    url(r'^avaliacoes/', include('ava360.urls')),
+
+    url(r'^register/', CreateView.as_view(
+            template_name='registration/register.html',
+            form_class=UserCreationForm,
+            success_url='/',
+    ), name='register',),
     
-    	url(r'^accounts/profile/$', 'project.views.dashboard',
-    	name='url_dashborard'),
+    url(r'^accounts/', include('django.contrib.auth.urls')),
 
-    	url(r'^logout/$', 'django.contrib.auth.views.logout',
-        	{'template_name': 'registration/logout.html'}, name='url_logout'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout',
+        {'template_name': 'registration/logout.html'}, name='url_logout'),
 
-    	url(r'^accounts/login/$', 'django.contrib.auth.views.login',
-        	{'template_name': 'index.html'}, name='url_login'),
+    url(r'^accounts/profile/$', 'project.views.dashboard',
+    name='url_dashborard'),
 
-    	# Change Password URLs:
-    	url(r'^accounts/password_change/$', 
-        	'django.contrib.auth.views.password_change', 
-        	{'post_change_redirect' : '/accounts/password_change/done/',
-        	'template_name':'registration/password_change_form.html'}, 
-        	name="password_change"), 
-    
-    	url(r'^accounts/password_change/done/$', 
-        	'django.contrib.auth.views.password_change_done'),
+    url(r'^change-password/$', 'django.contrib.auth.views.password_change', {
+    'template_name': 'registration/password_change_form.html'}, name="password-change"),
 
-    	#URL reset
-    	url(r'^reset/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$',
-        	'project.views.reset_confirm', name='reset_confirm'),
-    	url(r'^reset/$', 'project.views.reset', name='reset'),
-    	url(r'^reset_succeful/$', 'project.views.password_confirmation', name='url_reset_suceful'),             
-    	#url admin
-             url(r'^admin/', include(admin.site.urls)),   
+    url(r'^change-password-done/$', 'django.contrib.auth.views.password_change_done', {
+    'template_name': 'registration/password_change_done.html'
+    }, name="password-change-done"),
+
+    url(r'^admin/', include(admin.site.urls)),
 ]
