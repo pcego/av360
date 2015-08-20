29d1b8b525c011e821626afc034f5a63b330bfe0
pcego <pcego36@gmail.com>
2015-08-19 22:39:38 -0300

    refactoring

--- project/settings.py
+++ project/settings.py
@@ -153,16 +153,12 @@ STATICFILES_FINDERS = (
 )
 
 STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    
     os.path.join(BASE_DIR,"static"),
 )
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    
     os.path.join(BASE_DIR, 'templates'),
 
 )
